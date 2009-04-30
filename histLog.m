83a19923782fa115c208c19fdab8fdfe0145e4ca
Rodrigo Coura Torres <rtorres@rct.local>
2009-04-29 21:44:10 -0300

    histLog plota graficos em vetores de celulas.

--- histLog.m
+++ histLog.m
@@ -1,8 +1,28 @@
 function histLog(data, nBims)
 %function histLog(data, nBims)
 %Produces the histogram of data, using nBims (just like hist), but the y
-%axis is presented in log scale.
+%axis is presented in log scale. If data is a cell vector, then there will 
+%be one color for each cell. In that case, each cell MUST be a vector, not a matrix.
+%
 
-[y,x] = hist(data, nBims);
-bar(x,y,1);
-set(gca, 'YScale', 'log')
+if nargin == 1,
+  nBims = 10;
+elseif (nargin > 2) || (nargin < 1)
+  error('Invalid number of input parameters. See help.');
+end
+
+if iscell(data),
+  N = length(data);
+  x = zeros(nBims, N);
+  y = zeros(nBims, N);
+  for i=1:N,
+    [yi,xi] = hist(data{i}, nBims);
+    x(:,i) = xi;
+    y(:,i) = yi;
+  end
+else
+  [y,x] = hist(data, nBims);
+end
+
+bar(x,y,1,'grouped');
+set(gca, 'YScale', 'log');
