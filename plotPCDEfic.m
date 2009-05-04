16b8448fa6e544de56848a48bbbfbf6c3e56f84b
Rodrigo Coura Torres <rtorres@rct.local>
2009-05-04 17:59:04 -0300

    Adicionada fucnao p/ plotar a eficiencia retornada pelo npcd.

---
+++ plotPCDEfic.m
@@ -0,0 +1,16 @@
+function plotPCDEfic(ef)
+%function plotPCDEfic(ef)
+%Plota a vricao da eficiencia pelo numerod e PCDs, recebendo a estrutura de
+%eficiencia retornada pelo npcd (xxx.efic) contendo o maximo SP obtido p/c
+%ada SP, bem como o SP medio e seu desvio.
+%
+
+plot(ef.max, 'r*--');
+hold on;
+errorbar(ef.mean, ef.std, 'b*--');
+hold off;
+title('SP Variation over PCD');
+xlabel('# PCD');
+ylabel('SP (norm)');
+legend('Max SP', 'Mean SP');
+grid on;
