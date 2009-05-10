5cad9ed0d46cec526731a9512d684619f42febc3
Rodrigo Coura Torres <torres@sion.lps.ufrj.br>
2009-05-10 02:29:56 -0300

    ok

--- scramble.m
+++
@@ -1,7 +0,0 @@
-function x=scramble(init, end)
-%function x=scramble(init, end)
-%Generates a vector with values between [init,end], randomly placed.
-%
-%x=scramble(end)
-%Generates a vector with values between [1,end], randomly placed.
-%
