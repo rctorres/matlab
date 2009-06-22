8c229c3fe521cf94bfcb1197db2ef9d72a24b973
Rodrigo Coura Torres <torres@meyrin.lps.ufrj.br>
2009-06-22 18:18:01 -0300

    If no parameter is passed, reads from ../globals.mat.

--- load4Train.m
+++ load4Train.m
@@ -3,8 +3,13 @@ function [inTrn, inVal, inTst, ringsDist] = load4Train(globalInfo)
 %Loads the dataset files, and organize them already into train, val e test
 %sets. This script is intelligent enough to read data in UBUNTU and also MAC OS,
 %by reading the environment variable "OSTYPE". The path information is read from the variable
-%DATAPATH or DATAPATH_MAC, depending on the operating system being used.
+%DATAPATH or DATAPATH_MAC, depending on the operating system being used. If globalInfo is
+%ommited, it will try access the file stored in ../globals.mat
 %
+
+if nargin == 0,
+  globalInfo = '../globals.mat';
+end
 
 name = getenv('LOGNAME');
 if strcmp(name, 'torres'), %If ubuntu, logname is torres, otherwise os rtorres
