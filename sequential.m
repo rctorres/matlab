836917b07393364a069b65ded0208bce93c6906e
Rodrigo Coura Torres <torres@meyrin.lps.ufrj.br>
2009-07-22 13:00:44 -0300

    Adicionada info de debug ao sequenciador.

--- sequential.m
+++ sequential.m
@@ -1,4 +1,4 @@
-function nData = sequential(data, ringsDist, stopEnergy, energyThreshold, fixNorm, layers2Normalize)
+function [nData, dbg] = sequential(data, ringsDist, stopEnergy, energyThreshold, fixNorm, layers2Normalize)
 %function nData = sequential(data, ringsDist, stopEnergy, energyThreshold, fixNorm, layers2Normalize)
 %Realiza a normalizacao sequencial.
 %
@@ -35,20 +35,25 @@ end
 
 
 nData = zeros(size(data));
+dbg = cell(1,length(ringsDist));
 
 for i=1:length(ringsDist),  
   %Pegando os aneis da camada i.
   [iPos, ePos] = getLayerLimits(ringsDist, i);
   
   fprintf('Normalizando a camada %d.\n', i);
-  nData(iPos:ePos,:) = doLayer(data(iPos:ePos,:), stopEnergy(i), energyThreshold(i), fixNorm(i), layers2Normalize(i));
+  [nData(iPos:ePos,:), dbg{i}] = doLayer(data(iPos:ePos,:), stopEnergy(i), energyThreshold(i), fixNorm(i), layers2Normalize(i));
 end
 
 
-function nData = doLayer(data, stopEnergy, energyThreshold, fixNorm, doSequential)
+function [nData, dbg] = doLayer(data, stopEnergy, energyThreshold, fixNorm, doSequential)
   fprintf('For layer with %d rings, stop_energy = %f, energy_threshold = %f, fixed_norm = %f, do_sequential = %d\n', size(data,1), stopEnergy, energyThreshold, fixNorm, doSequential);
   nData = zeros(size(data));
   [nRings, nEvents] = size(data);
+  
+  dbg.fix = 0;
+  dbg.norm = 0;
+  dbg.seq = 0;
   
   for i=1:nEvents,
     rings = data(:,i);
@@ -65,12 +70,12 @@ function nData = doLayer(data, stopEnergy, energyThreshold, fixNorm, doSequentia
       norm(1) = max( norm(1), max(abs(rings)) );
     
       if norm(1) < energyThreshold,
-%        fprintf('Layer energy (%f) is less than the energy threshold. I will use the fixed_norm for everybody...\n', norm(1));
+        dbg.fix = dbg.fix + 1;
         nData(:,i) = rings ./ fixNorm;
         continue;
       end
       
-%      fprintf('Layer with energy (%f) less than stop_energy. Dividing by the total layer energy...\n', norm(1));
+      dbg.norm = dbg.norm + 1;
       nData(:,i) = rings ./ norm(1);
       continue;
     end
@@ -81,6 +86,8 @@ function nData = doLayer(data, stopEnergy, energyThreshold, fixNorm, doSequentia
       if (fixed) || (norm(j) < stopEnergy),
         norm(j) = norm(1);
         fixed = true;
+      else
+        dbg.seq = dbg.seq + 1;
       end
     end
     
