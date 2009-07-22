3c379bb7ccece9e4b13487c6a00841b8525f997b
Rodrigo Coura Torres <torres@meyrin.lps.ufrj.br>
2009-07-21 21:59:31 -0300

    More controllable version of the sequential norm.

--- sequential.m
+++ sequential.m
@@ -1,25 +1,38 @@
-function nData = sequential(data, ringsDist, stopEnergy, energyThreshold, layers2Normalize)
-%function nData = sequential(data, ringsDist, stopEnergy, energyThreshold, layers2Normalize)
+function nData = sequential(data, ringsDist, stopEnergy, energyThreshold, fixNorm, layers2Normalize)
+%function nData = sequential(data, ringsDist, stopEnergy, energyThreshold, fixNorm, layers2Normalize)
 %Realiza a normalizacao sequencial.
 %
 
 if nargin == 2,
   stopEnergy = 100;
   energyThreshold = 0.001;
+  fixNorm = 1000;
   layers2Normalize = ones(size(ringsDist));
 elseif nargin == 3,
   energyThreshold = 0.001;
+  fixNorm = 1000;
   layers2Normalize = ones(size(ringsDist));
 elseif nargin == 4,
+  fixNorm = 1000;
+  layers2Normalize = ones(size(ringsDist));
+elseif nargin == 5,
   layers2Normalize = ones(size(ringsDist));
 end
 
 fprintf('Realizando a normalizacao sequencial.\n');
-fprintf('Energy Threshold = %f\n', energyThreshold);
 
 if length(stopEnergy == 1),
   stopEnergy = repmat(stopEnergy,1,length(ringsDist));
 end
+
+if length(energyThreshold == 1),
+  energyThreshold = repmat(energyThreshold,1,length(ringsDist));
+end
+
+if length(fixNorm == 1),
+  fixNorm = repmat(fixNorm,1,length(ringsDist));
+end
+
 
 nData = zeros(size(data));
 
@@ -27,51 +40,49 @@ for i=1:length(ringsDist),
   %Pegando os aneis da camada i.
   [iPos, ePos] = getLayerLimits(ringsDist, i);
   
-  fprintf('Normalizando a camada %d. Vou fazer a parte sequencial? %d\n', i, layers2Normalize(i));
-  nData(iPos:ePos,:) = doLayer(data(iPos:ePos,:), stopEnergy(i), energyThreshold, layers2Normalize(i));
+  fprintf('Normalizando a camada %d.\n', i);
+  nData(iPos:ePos,:) = doLayer(data(iPos:ePos,:), stopEnergy(i), energyThreshold(i), fixNorm(i), layers2Normalize(i));
 end
 
 
-function nData = doLayer(data, stopEnergy, energyThreshold, doSequential)
-  fprintf('For layer with %d rings, stop_energy = %f\n', size(data,1), stopEnergy);
-  nData = data;
+function nData = doLayer(data, stopEnergy, energyThreshold, fixNorm, doSequential)
+  fprintf('For layer with %d rings, stop_energy = %f, energy_threshold = %f, fixed_norm = %f, do_sequential = %d\n', size(data,1), stopEnergy, energyThreshold, fixNorm, doSequential);
+  nData = zeros(size(data));
   [nRings, nEvents] = size(data);
   
   for i=1:nEvents,
     rings = data(:,i);
+    
+    if ~doSequential,
+      nData(:,i) = rings ./ fixNorm;
+      continue;
+    end
+    
     norm = zeros(nRings,1);
     norm(1) = abs(sum(rings));
     
     if norm(1) < stopEnergy,
-      max_r = max(rings);
-      min_r = min(rings);
-      if norm(1) < max_r,
-        new_norm = abs(max_r);
-        if abs(min_r) > new_norm,
-          new_norm = abs(min_r);
-        end
-        norm(1) = new_norm;
-      end
-      
+      norm(1) = max( norm(1), max(abs(rings)) );
+    
       if norm(1) < energyThreshold,
+        fprintf('Layer energy (%f) is less than the energy threshold. I will use the fixed_norm for everybody...\n', norm(1));
+        nData(:,i) = rings ./ fixNorm;
         continue;
       end
       
+      fprintf('Layer with energy (%f) less than stop_energy. Dividing by the total layer energy...\n', norm(1));
       nData(:,i) = rings ./ norm(1);
       continue;
     end
     
-    if doSequential,
-      fixed = false;
-      for j=2:nRings,
-        norm(j) = abs(norm(j-1) - abs(rings(j-1)));
-        if (fixed) || (norm(j) < stopEnergy),
-          norm(j) = norm(1);
-          fixed = true;
-        end
+    fixed = false;
+    for j=2:nRings,
+      norm(j) = abs(norm(j-1) - abs(rings(j-1)));
+      if (fixed) || (norm(j) < stopEnergy),
+        norm(j) = norm(1);
+        fixed = true;
       end
-      nData(:,i) = rings ./ norm;
-    else
-      nData(:,i) = rings ./ norm(1);
     end
+    
+    nData(:,i) = rings ./ norm;
   end
