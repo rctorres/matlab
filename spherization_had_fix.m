239086b5d34a10895c98db86b472971a02fa36dd
Rodrigo Coura Torres <rtorres@rct.local>
2009-10-01 14:24:26 -0300

    Esf. Fixa.

---
+++ spherization_had_fix.m
@@ -0,0 +1,30 @@
+function [otrn, oval, otst] = spherization_had_fix(trn, val, tst)
+%function [trn, val, tst] = spherization_had_fix(trn, val, tst)
+%Faz a normalizacao por esferizacao, dividindo a secao hadronica por
+%um valor fixo. trn, val, tst precisam ser vetores de celulas, com 
+%1 celula p/ cada classe.
+%
+  disp('Fazendo normalizacao por Esferizacao com Hadronica Fixa.');
+  
+  hadFixVal = 1000; %Fator de normalizacao da secao hadronica.
+  hadInitIdx = 89; % Indice onde comeca a secao hadronica.
+  
+  [aux, ps] = mapstd(cell2mat(trn));
+  clear aux;
+  
+  %Colocando a media = 0 e std = hadFix para os indices correspondentes a aneis
+  %pertencentes a secao hadronica. Assim, o proprio mapstd se encarregara
+  %da normalizacao correta.
+  ps.xmean(hadInitIdx:end) = 0;
+  ps.xstd(hadInitIdx:end) = hadFixVal;
+  
+  nClasses = length(trn);
+  otrn = cell(1, nClasses);
+  oval = cell(1, nClasses);
+  otst = cell(1, nClasses);
+  
+  for i=1:nClasses,
+    otrn{i} = mapstd('apply', trn{i}, ps);
+    oval{i} = mapstd('apply', val{i}, ps);
+    otst{i} = mapstd('apply', tst{i}, ps);
+  end
