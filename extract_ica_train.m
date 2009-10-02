69b63cb3ea29954a3af1e220e8ca91551a197ff3
Rodrigo Coura Torres <rtorres@rct.local>
2009-10-02 19:55:45 -0300

    Removido otrn do extract_ica_train tb.

--- extract_ica_train.m
+++ extract_ica_train.m
@@ -1,5 +1,5 @@
-function [otrn, oval, otst, pp] = extract_ica_train(trn, val, tst, par)
-%function [otrn, oval, otst, pp] = extract_ica_train(trn, val, tst, par)
+function [trn, val, tst, pp] = extract_ica_train(trn, val, tst, par)
+%function [trn, val, tst, pp] = extract_ica_train(trn, val, tst, par)
 %Extrai as ICAS da maneira correta p/ serem usadas no desenvolvimento de um
 %classificador. par deve ser uma estrutura contendo os seguintes campos:
 % - norm : ponteiro p/ a funcao usada p/ normalizar (event, esf, etc)
@@ -10,10 +10,10 @@ function [otrn, oval, otst, pp] = extract_ica_train(trn, val, tst, par)
 disp('Preparando os Conjuntos para Treino Com ICA');
 
 %Usando a normalizacao solicitada.
-[otrn, oval, otst, pp{1}] = par.norm(trn, val, tst);
+[trn, val, tst, pp{1}] = par.norm(trn, val, tst);
 
 %Removendo a media.
-[otrn, oval, otst, pp{2}] = remove_mean(otrn, oval, otst);
+[trn, val, tst, pp{2}] = remove_mean(trn, val, tst);
 
 %Extraindo as ICAs
 pp{3}.W = extract_ica(trn, par.ringsDist);
@@ -24,4 +24,4 @@ else
 end
 
 %Fasendo a projecao nas ICAs 
-[otrn, oval, otst] = do_projection(otrn, oval, otst, pp{3}.W, par.ringsDist);
+[trn, val, tst] = do_projection(trn, val, tst, pp{3}.W, par.ringsDist);
