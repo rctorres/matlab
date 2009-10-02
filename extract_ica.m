fd7dd76be062c67b587a2a9e76944fecbb886d54
Rodrigo Coura Torres <torres@meyrin.lps.ufrj.br>
2009-10-02 11:07:46 -0300

    ringsDist agora determinas e a extracao sera segmentada ou nao segmentada.

--- extract_ica.m
+++ extract_ica.m
@@ -1,16 +1,18 @@
-function W = extract_ica(trn, isSegmented, ringsDist)
-%function W = extract_ica(trn, isSegmented)
-%Extrai as ICAs. Se isSegmented = false, a extracao sera nao segmentada. 
+function W = extract_ica(trn, ringsDist)
+%function W = extract_ica(trn, ringsDist)
+%Extrai as ICAs. Se ringsDist = [], ou omitido, a extracao sera nao segmentada. 
 %Do contrario, a extracao sera feita camada a camada, e W sera 
-%um vetor de celulas, com as projecoes. Se ringsDist for omitido, a funcao
-%usara o default [8 64 8 8 4 4 4].
+%um vetor de celulas, com as projecoes.
 %
 
-if nargin < 3, ringsDist = [8 64 8 8 4 4 4]; end
+if nargin < 2, ringsDist = []; end
 
 data = cell2mat(trn);
 
-if isSegmented,
+if isempty(ringsDist),
+  fprintf('Extraindo as %d ICAs do caso NAO segmentado.\n', size(data,1));
+  W = jadeica(data);
+else
   N = length(ringsDist);
   W = cell(1,N);
   for i=1:N,
@@ -18,7 +20,4 @@ if isSegmented,
     fprintf('Extraindo as %d ICAs do caso segmentado.\n', size(ldata,1));
     W{i} = jadeica(ldata);
   end  
-else
-  fprintf('Extraindo as %d ICAs do caso NAO segmentado.\n', size(data,1));
-  W = jadeica(data);
 end
--- extract_pca.m
+++ extract_pca.m
@@ -1,17 +1,20 @@
-function W = extract_pca(trn, isSegmented, ringsDist)
-%function W = extract_pca(trn, isSegmented)
-%Extrai as PCAs. Se isSegmented = false, a extracao sera nao segmentada. 
+function W = extract_pca(trn, ringsDist)
+%function W = extract_pca(trn, ringsDist)
+%Extrai as PCAs. Se ringsDist = [], ou omitido, a extracao sera nao segmentada. 
 %Do contrario, a extracao sera feita camada a camada, e W sera 
 %um vetor de celulas, com as projecoes. Se ringsDist for omitido, a funcao
 %usara o default [8 64 8 8 4 4 4]. As projecoes serao retornadas nas LINHAS
 %de W.
 %
 
-if nargin < 3, ringsDist = [8 64 8 8 4 4 4]; end
+if nargin < 2, ringsDist = []; end
 
 data = cell2mat(trn);
 
-if isSegmented,
+if isempty(ringsDist),
+  fprintf('Extraindo as %d PCAs do caso NAO segmentado.\n', size(data,1));
+  W = do_job(data);
+else
   N = length(ringsDist);
   W = cell(1,N);
   for i=1:N,
@@ -19,9 +22,6 @@ if isSegmented,
     fprintf('Extraindo as %d PCAs do caso segmentado.\n', size(ldata,1));
     W{i} = do_job(ldata);
   end  
-else
-  fprintf('Extraindo as %d PCAs do caso NAO segmentado.\n', size(data,1));
-  W = do_job(data);
 end
 
 function proj = do_job(data)
