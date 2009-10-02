b7d5ab626ed66baa8b149764268a5781da5c33c7
Rodrigo Coura Torres <torres@meyrin.lps.ufrj.br>
2009-10-01 22:34:53 -0300

    iDebugadas.

--- extract_ica.m
+++ extract_ica.m
@@ -1,23 +1,24 @@
-function W = extract_ica(trn, ringsDist)
-%function W = extract_ica(trn, ringsDist)
-%Extrai as ICAs. Se ringsDist = [], ou omitido, a extracao sera naos
-%egmentada. Do contrario, a extracao sera feita camada a camada, e W sera 
-%um vetor de celulas, com as projecoes.
+function W = extract_ica(trn, isSegmented, ringsDist)
+%function W = extract_ica(trn, isSegmented)
+%Extrai as ICAs. Se isSegmented = false, a extracao sera nao segmentada. 
+%Do contrario, a extracao sera feita camada a camada, e W sera 
+%um vetor de celulas, com as projecoes. Se ringsDist for omitido, a funcao
+%usara o default [8 64 8 8 4 4 4].
 %
 
-if nargin < 2, ringsDist = []; end
+if nargin < 3, ringsDist = [8 64 8 8 4 4 4]; end
 
 data = cell2mat(trn);
 
-if isempty(ringsDist),
-  fprintf('Extraindo as %d ICAs do caso NAO segmentado.\n', size(data,1));
-  W = jade(data);
-else
+if isSegmented,
   N = length(ringsDist);
   W = cell(1,N);
   for i=1:N,
     fprintf('Extraindo as %d ICAs do caso segmentado.\n', size(ldata,1));
     ldata = getLayer(data, ringsDist, i);
-    W{i} = jade(ldata);
-  end
+    W{i} = jadeica(ldata);
+  end  
+else
+  fprintf('Extraindo as %d ICAs do caso NAO segmentado.\n', size(data,1));
+  W = jadeica(data);
 end
