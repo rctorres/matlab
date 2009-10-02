780550c14d2acd2d15b9843fce37332575dd251c
Rodrigo Coura Torres <torres@meyrin.lps.ufrj.br>
2009-10-02 16:23:27 -0300

    passando trainParam ao inves de batch_size.

--- train_all_cases.m
+++ train_all_cases.m
@@ -1,11 +1,12 @@
-function train_all_cases(trn, val, tst, norm, discover_via_pcd, batch_size, tst_equal_val)
-%function train_all_cases(trn, val, tst, norm, discover_via_pcd, batch_size, tst_equal_val)
+function train_all_cases(trn, val, tst, norm, discover_via_pcd, trainParam, tst_equal_val)
+%function train_all_cases(trn, val, tst, norm, discover_via_pcd, trainParam, tst_equal_val)
 %Treina todos os casos de pre-processamento passados.
 %trn, val, tst sao dos datasets. norm e a lista de normalizacos a serem
 %executadas. Atraves de norm, sera chamada a funcao com os parametros a
 %serem usados em cada normalizacao. discover_via_pcd, se true, vai fazer o sistema
 %realizar uma analise por PCD, p/ saber qual o melhor numero de nos na
-%camada escondida. batch_sise e o tamanho da batelada a ser usada, e
+%camada escondida. trainParam sao os parametros de treinamento, tal que
+%net.trainParam =  trainParam seja valido.
 %tst_equal_val, se true, diz a funcao que o conjunto de teste e identico ao
 %de validacao. Se discover_via_pcd = false, um fisher sera rodado, e, em
 %ambos os casos (fisher, net), a validacao cruzada sera feita.
@@ -13,7 +14,7 @@ function train_all_cases(trn, val, tst, norm, discover_via_pcd, batch_size, tst_
 
 for i=1:length(norm),
   par = get_parameters(norm{i});
-  [net, fisher] = train_case(trn, val, tst, par, discover_via_pcd, batch_size, tst_equal_val);
+  [net, fisher] = train_case(trn, val, tst, par, discover_via_pcd, trainParam, tst_equal_val);
   
   if discover_via_pcd,
     save(sprintf('nodes_analysis_%s.mat', par.id), 'net');
--- train_case.m
+++ train_case.m
@@ -1,11 +1,12 @@
-function [net, fisher] = train_case(trn, val, tst, par, discover_via_pcd, batch_size, tst_equal_val)
-%function [net, fisher] = train_case(trn, val, tst, par, discover_via_pcd, batch_size, tst_equal_val)
+function [net, fisher] = train_case(trn, val, tst, par, discover_via_pcd, trainParam, tst_equal_val)
+%function [net, fisher] = train_case(trn, val, tst, par, discover_via_pcd, trainParam, tst_equal_val)
 %Faz o treino de um caso especifico.
 %trn, val, tst sao dos datasets. par e a estrutura de treino especifica
 %para este caso, retornada pela funcao get_parameters existento no
 %diretorio de analise. discover_via_pcd, se true, vai fazer o sistema
 %realizar uma analise por PCD, p/ saber qual o melhor numero de nos na
-%camada escondida. batch_sise e o tamanho da batelada a ser usada, e
+%camada escondida. trainParam e a estrutura com os parametros de
+%treinamento, de tal forma que net.trainParam = trainParam seja valido.
 %tst_equal_val, se true, diz a funcao que o conjunto de teste e identico ao
 %de validacao. Se discover_via_pcd = false, um fisher sera rodado, e, em
 %ambos os casos (fisher, net), a validacao cruzada sera feita.
@@ -20,13 +21,13 @@ function [net, fisher] = train_case(trn, val, tst, par, discover_via_pcd, batch_
   else
     %Fazendo Fisher.
     numNodes = 0;
-    fisher = fullTrain(trn, val, tst, batch_size, numNodes, par, tst_equal_val);
+    fisher = fullTrain(trn, val, tst, trainParam, numNodes, par, tst_equal_val, par.doCrossVal);
     fisher.desc = par.desc;
     fisher.id = par.id;
     numNodes = par.hidden_nodes; %P/ o treino neural.
   end
 
   %Treino neural.
-  net = fullTrain(trn, val, tst, batch_size, numNodes, par, tst_equal_val);
+  net = fullTrain(trn, val, tst, trainParam, numNodes, par, tst_equal_val, par.doCrossVal);
   net.desc = par.desc;
   net.id = par.id;
