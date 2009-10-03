48ea7eb46c45e452e26d5c6a8b329e759c8cea43
Rodrigo Coura Torres <torres@meyrin.lps.ufrj.br>
2009-10-02 21:25:15 -0300

    Prtotipod a funcao p/ fazer ICA-PCA.

---
+++ extract_ica_pca_train.m
@@ -0,0 +1,59 @@
+function [trn, val, tst, pp] = extract_ica_pca_train(trn, val, tst, par)
+%function [trn, val, tst, pp] = extract_ica_pca_train(trn, val, tst, par)
+%Extrai as ICAS da maneira correta p/ serem usadas no desenvolvimento de um
+%classificador, reduzindo a domensao dos eventos via PCA.
+%par deve ser uma estrutura contendo os seguintes campos:
+% - norm : ponteiro p/ a funcao usada p/ normalizar (event, esf, etc)
+% - ringsDist : vetor com a distribuicao dos aneis. Se for [], a extracaos
+% - nComp : um valor (caso nao-segmentado), ou um vetor, especificando o
+%           numero de PCs p/ serem retidas do evento ou de cada segmento. 
+%           Se este campo for [], TODAS as PCs serao utilizadas.
+%
+% era NAO segmentada. Do contrario, faco a extracao segmentada.
+%
+
+disp('Preparando os Conjuntos para Treino Com ICA Compactadas por PCA');
+
+%Usando a normalizacao solicitada.
+[trn, val, tst, pp{1}] = par.norm(trn, val, tst);
+
+%Removendo a media.
+[trn, val, tst, pp{2}] = remove_mean(trn, val, tst);
+
+%Extraindo as PCAs
+pp{3} = extract_pca(trn, par.ringsDist);
+if isempty(par.ringsDist),
+  pp{3}.name = 'PCA';  
+else
+  pp{3}.name = 'PCA-Seg';
+end
+
+%Fazendo a compactacao do sinal, se solicitado.
+W = do_reduction(pp{3}.W, par.ringsDist, par.nComp);
+
+%Fazendo a projecao nas PCAs 
+[trn, val, tst] = do_projection(trn, val, tst, W, par.ringsDist);
+
+%Extraindo as ICAs
+pp{3}.W = extract_ica(trn, par.nComp);
+if isempty(par.ringsDist),
+  pp{3}.name = 'ICA';
+else
+  pp{3}.name = 'ICA-Seg';
+end
+
+%Fasendo a projecao nas ICAs 
+[trn, val, tst] = do_projection(trn, val, tst, pp{3}.W, par.nComp);
+
+
+function W = do_reduction(pca, ringsDist, nComp)
+  disp('Reduzindo a dimensao do evento via PCA');
+  if isempty(ringsDist), %Nao segmentado
+    W = pca(1:nComp,:);
+  else %Segmentado
+    N = length(pca);
+    W = cell(1,N);
+    for i=1:N,
+      W{i} = pca{i}(1:nComp(i),:);
+    end
+  end
