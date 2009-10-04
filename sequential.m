3670e6598c6a227655690dc6e0dc455ea3f1f27b
Rodrigo Coura Torres <rtorres@rct-w.(none)>
2009-10-04 20:42:31 -0300

    Voltei p/ versao antiga.

--- sequential.m
+++ sequential.m
@@ -1,15 +1,58 @@
-function [trn, val, tst, pp] = sequential(trn, val, tst, par)
-%function [trn, val, tst, pp] = sequential(trn, val, tst, ringsDist, par)
-%Realiza a normalizacao sequencial. par precisa ter os seguintes campos:
-% - ringsDist : Numero de aneis em cada camada.
-% - secDist : vetor de celulas indicando, por uma string, se a camada e EM
-%             ou HD.
+function [trn, val, tst] = sequential(trn, val, tst, ringsDist)
+%function [trn, val, tst] = sequential(trn, val, tst, ringsDist)
+%Realiza a normalizacao sequencial.
 %
-  pp.name = 'sequential';
-  disp('Fazendo normalizacao por evento.');
   fprintf('Fazendo normalizacao sequencial.\n');
   for i=1:length(trn),
-    trn{i} = ringer_norm(trn{i}, par.ringsDist, par.secDist, pp.name);
-    val{i} = ringer_norm(val{i}, par.ringsDist, par.secDist, pp.name);
-    tst{i} = ringer_norm(tst{i}, par.ringsDist, par.secDist, pp.name);
+    trn{i} = do_set(trn{i}, ringsDist);
+    val{i} = do_set(val{i}, ringsDist);
+    tst{i} = do_set(tst{i}, ringsDist);
+  end
+
+
+function [data] = do_set(data, ringsDist)
+
+  nLayers = length(ringsDist);
+
+  for i=1:nLayers,
+    %Pegando os aneis da camada i.
+    [ip, ep] = getLayerLimits(ringsDist, i);  
+    data(ip:ep,:) = doLayer(data(ip:ep,:));
+  end
+
+
+function [nData] = doLayer(data)
+  stopEnergy = 100;
+  energyThreshold = 0.001;
+
+  nData = zeros(size(data));
+  [nRings, nEvents] = size(data);
+  
+  for i=1:nEvents,
+    rings = data(:,i);
+    
+    norm = zeros(nRings, 1); 
+    norm(1) = abs(sum(rings));
+    
+    if norm(1) < stopEnergy,
+      norm(1) = max( norm(1), max(abs(rings)) );
+    
+      if norm(1) < energyThreshold,
+        continue;
+      end
+      
+      nData(:,i) = rings ./ norm(1);
+      continue;
+    end
+    
+    fixed = false;
+    for j=2:nRings,
+      norm(j) = abs(norm(j-1) - rings(j-1));
+      if (fixed) || (norm(j) < stopEnergy),
+        norm(j) = norm(1);
+        fixed = true;
+      end
+    end
+    
+    nData(:,i) = rings ./ norm;
   end
