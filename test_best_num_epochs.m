2be3347299fe2da90145c9af90d03661b7c31601
Rodrigo Coura Torres <torres@meyrin.lps.ufrj.br>
2009-10-07 14:26:55 -0300

    Imprimindo a variacao em SP emc ada slot.

--- test_best_num_epochs.m
+++ test_best_num_epochs.m
@@ -14,6 +14,7 @@ N = length(trnEvo);
 nPt = floor(length(trnEvo{1}.epoch) / res);
 
 sp = zeros(N,nPt);
+conv = zeros(N,nPt);
 leg = cell(1,N);
 
 for i=1:N,
@@ -22,7 +23,10 @@ for i=1:N,
   fprintf('SP maximo (%2.2f) ocorrido na epoca %d\n', 100*spRef, evo.epoch(I));
   leg{i} = sprintf('Max SP = %2.2f', 100*spRef);
   for j=1:nPt,
-    sp(i,j) = 100*(spRef - max(evo.sp_val(1:j*res))) / spRef; 
+    sp(i,j) = 100*(spRef - max(evo.sp_val(1:j*res))) / spRef;
+    
+    slot = evo.sp_val(((j-1)*res)+1:(j*res));
+    conv(i,j) = 100*(max(slot) - min(slot)) / max(slot);
   end
 end
 
@@ -30,10 +34,25 @@ x = (1:res:1000*nPt);
 x = repmat(x, N, 1);
 
 figure;
+
+subplot(2,1,1);
+hold on;
 plot(x', sp');
 set(gca, 'xScale', 'log');
 grid on;
 title('Variacao do SP Maximo Obtido Com o Numero de Epocas de Treinamento');
 xlabel('Numero de Epocas');
+ylabel('Diferenca relativa (%)');
+legend(leg);
+
+conv(conv == 0) = 0.00001;
+subplot(2,1,2);
+hold on;
+plot(x', conv');
+set(gca, 'xScale', 'log');
+set(gca, 'yScale', 'log');
+grid on;
+title('Diferenca Relativa Entre o SP Maximo e Minimo em Cada Slot');
+xlabel('Epocas');
 ylabel('Diferenca relativa (%)');
 legend(leg);
