aba26d56be3736c0f7f40c0aae72452cb5959ba9
Rodrigo Coura Torres <torres@meyrin.lps.ufrj.br>
2009-10-07 12:38:39 -0300

    Funcao p/ plotar ROC, SP, Area da ROC e PD@PF.

---
+++ do_roc_plot.m
@@ -0,0 +1,77 @@
+function [h_roc, h_sp] = do_roc_plot(net, fisher, c, idx, pfRef)
+%function [h_roc, h_sp] = do_roc_plot(net, fisher, c, idx, pfRef)
+%Plota a ROC e a tabela de eficiencias recebendo os valores net e fisher
+%salvos por train_case. c e uma cor a representar o conjunto a ser plotado.
+%idx e o indice para ser usado no eixo x do errorbar, e pfRef e um falso
+%alarme de referencia [0-1] par aque se ja apresentada a detecao
+%correspondente.
+
+  disp(net.desc);
+  cases = {net, fisher};
+  traces = {'-', '--'};
+  marker = {'s', 'o'};
+  h_roc = zeros(1,2);
+  h_sp = zeros(1,2);
+  
+  for i=1:2,
+    d = cases{i};
+    t = traces{i};
+    m = marker{i};
+
+    %ROC
+    subplot(2,2,1);
+    hold on;
+    h = plot(100*d.fa', 100*d.det', [c t]);
+    grid on;
+    title('ROCs Aneis Puros');
+    xlabel('Falso Alarme (%)');
+    ylabel('Detecao (%)');
+    h_roc(i) = h(1);
+  
+    %SP
+    subplot(2,2,2);
+    hold on;
+    mv = mean(d.sp);
+    [si, ss] = adjustErrorRanges(mv, std(d.sp));
+    h = errorbar(idx-1+i, 100*mv, 100*si, 100*ss, [c m]);
+    grid on;
+    title('Maximo SP de Cada Caso');
+    xlabel('Caso');
+    ylabel('SP (%)');
+    h_sp(i) = h(1);
+
+    %Area da ROC
+    subplot(2,2,3);
+    hold on;
+    detSum = roc_area(d.det);
+    mv = mean(detSum);
+    [si, ss] = adjustErrorRanges(mv, std(detSum));
+    errorbar(idx-1+i, 100*mv, 100*si, 100*ss, [c m]);
+    grid on;
+    title('Area da ROC');
+    xlabel('Caso');
+    ylabel('Area');
+
+    %SP
+    subplot(2,2,4);
+    hold on;
+    [v, I] = min(abs(d.fa - pfRef), [], 2);
+    pd = d.det(I);
+    mpd = mean(d.det(I));
+    [si, ss] = adjustErrorRanges(mpd, std(pd));
+    errorbar(idx-1+i, 100*mpd, 100*si, 100*ss, [c m]);
+    grid on;
+    title(sprintf('P_D @ P_{FA} = %2.2f%%', 100*pfRef));
+    xlabel('Caso');
+    ylabel('Detecao (%)');
+
+ 
+    
+%    fprintf('%s   : SP = %2.2f, Area = %3.2f, Pd@Pf = %2.0f%% = %f\n', getNumNodesAsText(d.net), 100*d.sp, sum(d.det), 100*pfRef, pd);
+  end
+  
+  
+function s = roc_area(pd)
+  res = 1 / size(pd,2);
+  s = sum(res*pd, 2);
+  
