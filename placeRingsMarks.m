18355e8625681105c5423cf81f43d500555210db
Rodrigo Coura Torres <rtorres@rct.local>
2009-10-14 16:05:07 -0300

    Agora coloca a marca na direcao de Y.

--- placeRingsMarks.m
+++ placeRingsMarks.m
@@ -1,16 +1,22 @@
-function placeRingsMarks(ringsDist, minY, maxY)
+function placeRingsMarks(ringsDist, minV, maxV, doYDirection)
 %function placeRingsMarks(ringsDist, minY, maxY)
 %Sets the rings boundaries for each layer.
 %ringsDist ia a vector containing the number of rings for each layer. minY
 %and maxY are, respectively, the minimum and maximum values for the mark to
 %appear on the plot. This functions assumes that a plot with the rings
-%already exists, and has the current focus.
+%already exists, and has the current focus. doYDirection, if true (default
+%false), will place the rings marks along the Y axis.
 %
 
 hold on;
 beg = 0.5;
 for i=1:(length(ringsDist)-1),
   beg =  beg + ringsDist(i);
-  plot([beg beg], [minY maxY], 'k:');
+  
+  if doYDirection,
+    plot([minV maxV], [beg beg], 'k:');
+  else
+    plot([beg beg], [minV maxV], 'k:');
+  end
 end
 hold off;
