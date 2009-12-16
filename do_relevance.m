991793f96176227faf18a790208136a2c72db308
Rodrigo Coura Torres <rtorres@rct.local>
2009-12-16 18:47:53 -0200

    Removido ringsDist.

--- do_relevance.m
+++ do_relevance.m
@@ -1,5 +1,5 @@
-function [r_mse, r_sp] = do_relevance(net_str, ringsDist, trn, val, tst)
-%function [r_mse, r_sp] = do_relevance(net, ringsDist, trn, val, tst)
+function [r_mse, r_sp] = do_relevance(net_str, trn, val, tst)
+%function [r_mse, r_sp] = do_relevance(net, trn, val, tst)
 %Faz a analise da relevancia. net_str e uma estrutura tal como retornada
 %pelo fullTrain, contendo os pre-processamentos aplicados, a rede treinada,
 %e toda a informacao necessaria. Esta funcao retorna os valores de
@@ -9,7 +9,7 @@ function [r_mse, r_sp] = do_relevance(net_str, ringsDist, trn, val, tst)
   net = net_str.net;
   pp = net_str.pp;
   
-  [trn, val, tst] = do_pre_proc(pp, ringsDist, trn, val, tst);
+  [trn, val, tst] = do_pre_proc(pp, trn, val, tst);
   clear tst;
   
   r_mse = relevance(net, cell2mat(trn), cell2mat(val));
