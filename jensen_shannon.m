a6376b9f5c88c0fa3961fc22d6755dc7da2556f8
Rodrigo Coura Torres <rtorres@rct.local>
2010-01-08 11:01:52 -0200

    Dinstancia JS adicionada. KLDiv agora retorna a distancia simetrica.

--- jensen_shannon.m
+++ jensen_shannon.m
@@ -1,7 +1,7 @@
-function dist=KLDiv(P,Q, nbins)
-% dist=KLDiv(P,Q, nbins)
+function dist=jensen_shannon(P,Q, nbins)
+% dist=jensen_shannon(P,Q, nbins)
 %
-%Calculates the Kullback-Leibler divergence between the P and Q random
+%Calculates the jensen_shannon divergence between the P and Q random
 %variables. The p.d.fs are estimated by histogramming both variables with
 %the number of bins given in 'nbins'.
 %
@@ -18,8 +18,9 @@ function dist=KLDiv(P,Q, nbins)
   % normalizing the P and Q
   Q = Q ./ sum(Q);
   P = P ./ sum(P);
-  d1 =  safe_sum(P.*log(P./Q));
-  d2 =  safe_sum(Q.*log(Q./P));
+  M = mean([P;Q]);
+  d1 =  safe_sum(P.*log(P./M));
+  d2 =  safe_sum(Q.*log(Q./M));
 
   dist = (d1 + d2) / 2;
 
