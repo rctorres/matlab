b3a6def7ba3788437a45c90a143ee8bcbea74dc5
Rodrigo Coura Torres <rtorres@rct.local>
2010-02-09 12:59:23 -0200

    Deixando setar o num de ptos e o modo de estimacao da PDF.

--- entropy.m
+++ entropy.m
@@ -1,5 +1,9 @@
-function h = entropy(data, p)
+function h = entropy(data, p, mode, nPoints)
   
+  if nargin < 2, p = []; end
+  if nargin < 3, mode = 'kernel'; end
+  if nargin < 4, nPoints = 128; end
+
   if size(data,1) == 1,
     u = data(1,:);
     v = [];
@@ -12,10 +16,7 @@ function h = entropy(data, p)
     end
   end
 
-  if nargin < 2, %We must estimate the pdf
-    nPoints = 128;
-    mode = 'kernel';
-
+  if isempty(p), %We must estimate the pdf   
     %Calculando a estimativa da PDF pelo modo desejado.
     if strcmp(mode, 'hist'),
       p = get_pdf_by_hist(u, v, nPoints);
@@ -23,10 +24,7 @@ function h = entropy(data, p)
       p = get_pdf_by_kernel(u, v, nPoints);
     else
       error('Invalid mode! See help for options.')
-    end
-    
-    %Normalizando p/ o somatorio ser 1.
-    p = p ./ sum(sum(p));
+    end    
   end
   
   %Calculo a entropia considerando so os valores > 0 p/ evitar valor
@@ -44,6 +42,10 @@ function p = get_pdf_by_hist(u,v,nPoints)
     desc = [min(u) max(u), nPoints; min(v), max(v), nPoints];
     p = histogram2(u,v,desc);
   end
+  
+  %Normalizando p/ o somatorio ser 1.
+  p = p ./ sum(sum(p));
+
   
   
 function p = get_pdf_by_kernel(u,v,nPoints)
