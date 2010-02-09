f95cd5ef9dfff600b2d3cc30e7761f376c82933f
Rodrigo Coura Torres <rtorres@rct.local>
2010-02-09 13:32:29 -0200

    Melhorando o calculo da inf mutua.

--- mutual_info.m
+++ mutual_info.m
@@ -1,6 +1,8 @@
-function c = mutual_info(m, doNorm)
+function c = mutual_info(m, doNorm, mode, nPoints)
 
   if nargin < 2, doNorm = false; end;
+  if nargin < 3, mode = 'kernel'; end;
+  if nargin < 4, nPoints = 128; end;
 
   m = m';
   [M,N] = size(m);
@@ -10,15 +12,18 @@ function c = mutual_info(m, doNorm)
     x = m(i,:);
     for j=i:M,
       y = m(j,:);
-      c(i,j) = entropy(x) + entropy(y) - entropy([x;y]);
-    end
-    
-    if doNorm,
-      c(i,i:end) = c(i,i:end) ./ max(c(i,i:end));
-    end
+      hx = entropy(x, [], mode, nPoints);
+      hy = entropy(y, [], mode, nPoints);
+      hxy = entropy([x;y], [], mode, nPoints);
+      c(i,j) = hx + hy - hxy;
+    end    
   end
     
   
   %Componho a matriz final colocando a transposta da matriz na parte
   %inferior.
   c = triu(c) + tril(c',-1);
+
+  if doNorm,
+    c = ones(size(c)) - exp(-2*c);
+  end
